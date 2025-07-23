a7d208fd3eef9a87dce308201706ad679696ff43
Victor Langemaire <victor.langemaire@ecare.nl>
2025-07-23 22:33:32 +0200

    Update anonymacy version to 0.0.2 and clean up imports in anonymacy_demo.py

--- notebooks/anonymacy_demo.py
+++ notebooks/anonymacy_demo.py
@@ -2,7 +2,7 @@
 # requires-python = ">=3.12"
 # dependencies = [
 #     "marimo",
-#     "anonymacy==0.0.1",
+#     "anonymacy==0.0.2",
 #     "gliner-spacy==0.0.11",
 #     "nl-core-news-sm @ https://github.com/explosion/spacy-models/releases/download/nl_core_news_sm-3.8.0/nl_core_news_sm-3.8.0-py3-none-any.whl",
 #     "spacy==3.8.7",
@@ -20,19 +20,10 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    import sys
-    import os
-    from pathlib import Path
     import marimo as mo
-    from spacy import Language
-    from spacy.lang.nl import Dutch
-    from spacy.pipeline import SpanRuler
-    from spacy.tokens import Span
     from spacy import displacy
     import spacy
 
-    from anonymacy import ContextEnhancer
-    from anonymacy.recognizer import PatternRecognizer, BsnRecognizer
     return displacy, mo, spacy
 
 
@@ -90,7 +81,7 @@ def _(displacy, mo, spacy, text_area):
         }
     }
 
-    anonymizer = nlp.add_pipe("anonymizer", config=anonymizer_config)
+    nlp.add_pipe("anonymizer", config=anonymizer_config)
 
     text = text_area.value
     doc = nlp(text)
